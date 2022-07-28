f9c01d08af0a34df7e9e57993ffd2ccd37a4f317
Arnav Patel <arnavpatel09@gmail.com>
2022-07-27 21:56:38 -0700

    added CSS to the user profile page and modified create_profile_page accordingly

---
+++ js/error.js
@@ -0,0 +1,10 @@
+const firstName = document.getElementById("firstName").value
+const lastName = document.getElementById("lastName").value
+const phoneNumber = document.getElementById("phoneNumber").value
+const gender = document.getElementById("gender").value
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+const form = document.getElementById('form')
+
+
+form.addEventListener
